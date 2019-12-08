4a609a2fb064e87b8cbcd8f99458514d248cdeca
Em3rgencyLT <em3rgency.lt@gmail.com>
2019-12-08 22:28:38 +0200

    feat(Player): add prototype player controller assets

---
+++ Assets/Scripts/Scriptable Objects/PlayerSpawner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Spawners / Player Spawner")]
+public class PlayerSpawner : ScriptableObject
+{
+    [SerializeField] private GameObject _playerPrefab;
+
+    public void SpawnPlayer(Vector3 position)
+    {
+        Instantiate(_playerPrefab, position, Quaternion.identity);
+    }
+}
