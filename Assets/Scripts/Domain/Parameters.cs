e9d12c3df4d68498158d2ea366d8e08699203b45
Em3rgencyLT <em3rgency.lt@gmail.com>
2019-12-13 18:07:59 +0200

    feat(SRTM): add support for bilinear interpolation in SRTM library

--- Assets/Scripts/Domain/Parameters.cs
+++ Assets/Scripts/Domain/Parameters.cs
@@ -3,20 +3,17 @@ namespace Domain
     public static class Parameters
     {
         public static readonly string OSM_DATA_API_URL = @"https://overpass-api.de/api/map?bbox=";
-
         public static readonly string NASA_SRTM_USERNAME = "";
-
         public static readonly string NASA_SRTM_PASSWORD = "";
         /*The WORLD_SIZE_MULTIPLIER and HEIGHTMAP_RESOLUTION_BASE_POWER values correlate to world smoothness.
          Adjust if begin seeing rectangular tiling. Lowering either value will make things smoother. Lowering too
          much will decrease accuracy.*/
         //FIXME: A possible way to resolve this permanently would be to smooth the heightmap in code. This would not sacrifice height data.
         //FIXME: Another alternative is to find a higher resolution SRTM data source
-        public static readonly float WORLD_SIZE_MULTIPLIER = 0.006f;
-        public static readonly int HEIGHTMAP_RESOLUTION_BASE_POWER = 3;
+        public static readonly float WORLD_SIZE_MULTIPLIER = 0.01f;
         public static readonly int TERRAIN_LAYER = 15;
-        public static readonly int CHUNK_SIZE = 768;
-        public static readonly int TERRAIN_CHUNK_DISTANCE = 2;
+        public static readonly int CHUNK_SIZE = 512;
+        public static readonly int TERRAIN_CHUNK_DISTANCE = 1;
         public static readonly int MAP_CHUNK_DISTANCE = 5;
     }
 }
--- Assets/Scripts/Services/SRTMDataService.cs
+++ Assets/Scripts/Services/SRTMDataService.cs
@@ -2,6 +2,7 @@ using System.Net;
 using Domain;
 using SRTM;
 using SRTM.Sources.NASA;
+using SRTM.Sources.USGS;
 using Utility;
 
 namespace Services
@@ -12,14 +13,22 @@ namespace Services
         
         public SRTMDataService()
         {
-            var credentials = new NetworkCredential(Parameters.NASA_SRTM_USERNAME, Parameters.NASA_SRTM_PASSWORD);
-            _srtmData = new SRTMData(FolderPaths.SRTMData, new NASASource(credentials));
+            if (string.IsNullOrEmpty(Parameters.NASA_SRTM_USERNAME) ||
+                string.IsNullOrEmpty(Parameters.NASA_SRTM_PASSWORD))
+            {
+                _srtmData = new SRTMData(FolderPaths.SRTMData, new USGSSource());
+            }
+            else
+            {
+                var credentials = new NetworkCredential(Parameters.NASA_SRTM_USERNAME, Parameters.NASA_SRTM_PASSWORD);
+                _srtmData = new SRTMData(FolderPaths.SRTMData, new NASASource(credentials));
+            }
         }
 
         public double GetHeight(Coordinates coordinates)
         {
             //TODO: go over all -1 values in the heightmap and average them to their neighbour values
-            return _srtmData.GetElevation(coordinates.Latitude, coordinates.Longitude) ?? -1;
+            return _srtmData.GetElevationBilinear(coordinates.Latitude, coordinates.Longitude) ?? -1;
         }
     }
 }
